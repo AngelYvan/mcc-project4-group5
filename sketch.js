c37bdf3bac9fc8d739b1102ffad54818ff1265e9
GUSTAVOMANRIQUEF <111947375+GUSTAVOMANRIQUEF@users.noreply.github.com>
2022-09-03 11:52:44 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,21 +1,47 @@
-let qt;
-let count = 0;
+function setup () {
+var width = 250;
+ var height = 200;
+createCanvas (width , height ) ;
+ background (0) ;
+for (var x = 0; x < width; x += width / 10) {
+for (var y = 0; y < height; y += height / 5) {
+stroke (125 , 125 , 125) ;
+strokeWeight (1) ;
+line (x, 0, x, height );
+line (0 , y, width , y);
+}
+}
+var data = [];
+for ( let i = 0; i < 12; i ++) {
+var x = Math.floor ( Math.random () * height );
+var y = Math.floor ( Math.random () * height );
+data.push ([x, y]) ;
+fill (255 , 255 , 255) ;
+circle (x, height - y, 7) ; // 200 -y para q se dibuje apropiadamente
+textSize (8) ;
+text (x + ',' + y, x + 5, height - y);// 200 -y para q se dibuje
+apropiadamente
+}
+var root = build_kdtree ( data ) ;
+console.log ( root );
+}
+k = 2;
 
-function setup (){
-createCanvas (400 ,400) ;
+class Node {
+constructor (point , axis ){
+this.point = point;
+this.left = null;
+this.right = null;
+this.axis = axis;
+}
+ }
 
-// centre point and half of width and height
-let boundary = new Rectangle (200 ,200 ,200 ,200) ;
+function distanceSquared ( point1 , point2 ){
+var distance = 0;
+for (var i = 0; i < k; i ++)
+distance += Math.pow (( point1 [i] - point2 [i]) , 2) ;
+return Math.sqrt ( distance );
+ }
 
- // each leave just could have 4 elements
- qt = new QuadTree ( boundary , 4);
-
- console.log (qt);
- for (let i=0; i < 3; i ++) {
-  let p = new Point ( Math.random () * 400 , Math.random () * 400) ;
-  qt.insert (p);
-  }
- 
-  background (0);
-  qt.show ();
-  }
+ function closest_point_brute_force ( points , point ) {}
+ function naive_closest_point (node , point , depth = 0, best = null ) {}
