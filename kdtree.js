984f941b6cbbc52111fdb3219d131ddee2b4a370
Angel Yvan Choquehuanca Peraltilla <achoquehuancaper@unsa.edu.pe>
2022-09-11 12:39:03 -0500

    query circle - query rectangle

--- kdtree.js
+++ kdtree.js
@@ -10,10 +10,27 @@ class Node {
 }
 
 function getHeight(node) {
-
+    if(node == null)   
+        return 0;
+    else   
+        return 1 +(Math.max(getHeight(node.left),getHeight(node.right))); 
 }
 function generate_dot(node) {
+    var cad="";
+	if(node==null)
+		return "";
 
+	if(node.left!=null)
+	{
+		cad=cad+'"'+node.point.toString()+"\"";
+		cad=cad+" -> "+'"'+node.left.point.toString()+'"'+";"+"\n";
+	}
+	if(node.right!=null)
+	{
+		cad=cad+"\""+node.point.toString()+"\"";
+		cad=cad+" -> "+'"'+node.right.point.toString()+'"'+";"+"\n";
+	}
+	return cad+generate_dot(node.left)+generate_dot(node.right);
 }
 function build_kdtree(points, depth = 0) {
     var n = points.length;
@@ -45,4 +62,145 @@ function build_kdtree(points, depth = 0) {
     node.right = build_kdtree(right, depth + 1);
 
     return node;
+}
+
+
+
+function distanceSquared(point1, point2) {
+    var distance = 0;
+    for (var i = 0; i < k; i++)
+        distance += Math.pow((point1[i] - point2[i]), 2);
+    return Math.sqrt(distance);
+}
+
+function closest_point_brute_force(points, point) { 
+    var nearPoints = points[0];
+	var DistanceMin = distanceSquared(points[0], point);
+	for (var i = 1; i < points.length; i++) {
+		var t = distanceSquared(points[i], point);
+		if (DistanceMin > t) {
+			nearPoints = points[i];
+			DistanceMin = t;
+		}
+	}
+	return nearPoints;
+}
+function naive_closest_point(node, point, depth = 0, best = null) {
+	if (node != null) {
+		var dis = distanceSquared(node.point, point);
+		console.log(dis);
+		if (best != null && distanceSquared(best, point) < dis) {
+			return best;
+		}
+		else {
+			if (node.point[node.axis] > point[node.axis]) {
+				return naive_closest_point(node.left, point, depth + 1, node.point);
+			}
+			else {
+				return naive_closest_point(node.right, point, depth + 1, node.point);
+			}
+		}
+	}
+	else {
+		return best;
+	}
+}
+
+function closer_point(point, p1, p2) {
+	if (p2 == null) {
+		return p1;
+	}
+	var distance = distanceSquared(p1.point, point);
+	if (distance < distanceSquared(p2.point, point))
+		return p1;
+	return p2;
+}
+
+function closest_point(node, point, depth = 0) {
+
+	if (node === null)
+		return null;
+	var axis = depth % k;
+	var next_branch = null;
+	var opposite_branch = null;
+	if (point[axis] < node.point[axis]) {
+		next_branch = node.left;
+		opposite_branch = node.right;
+	} else {
+		next_branch = node.right;
+		opposite_branch = node.left;
+	}
+	var best = closer_point(point, node, closest_point(next_branch, point, depth + 1));
+	if (distanceSquared(point, best.point) > Math.abs(point[axis] - node.point[axis])) {
+		best = closer_point(point, best, closest_point(opposite_branch, point, depth + 1));
+	}
+
+	return best;
+}
+
+function KNN(points, point, K) {
+	var nearPoints = [];
+	var found = [];
+	for (var i = 0; i < points.length; i++) 
+	{
+		var aux=distanceSquared(points[i],point);
+		nearPoints.push([aux,points[i]])
+
+		nearPoints.sort(function (a,b){
+			return a[0]-b[0];
+		});
+	}
+	for(var i = 0; i < nearPoints.length; i++){
+		found.push(nearPoints[i].slice(1,2));
+	}
+	console.log(found.slice(0, K))
+}
+
+var p = 0;
+
+function range_query_circle(node, center, radio, queue, depth = 0) {
+	if (node == null) {
+		return null;
+	}
+	p += 1;
+	console.log(p);
+	var axis = node.axis;
+	var next_branch = null;
+	var opposite_branch = null;
+	if (center[axis] < node.point[axis]) {
+		next_branch = node.left;
+		opposite_branch = node.right;
+	} else {
+		next_branch = node.right;
+		opposite_branch = node.left;
+	}
+	var best = closer_point(center, node, range_query_circle(next_branch, center, radio, queue, depth + 1));
+	if (Math.abs(center[axis] - node.point[axis]) <= radio || distanceSquared(center, best.point) > Math.abs(center[axis] - node.point[axis])) {
+		if (distanceSquared(center, node.point) <= radio) {
+			queue.push(node.point);
+		}
+		best = closer_point(center, best, range_query_circle(opposite_branch, center, radio, queue, depth + 1));
+	}
+
+	return best;
+}
+
+function range_query_rect(node , rect , found , depth = 0) {
+	if (node === null) {
+        return;
+    }
+    var axis = depth % k;
+    if (node.point[axis] < rect[axis][0]){
+        range_query_rect(node.right, rect, found, depth+1)
+    }
+    if (node.point[axis] > rect[axis][1]) {
+        range_query_rect(node.left, rect, found, depth+1)
+    }
+    let x = node.point[0]
+    let y = node.point[1]
+    if (!(rect[0][0]>x || rect[0][1]<x || rect[1][0]>y || rect[1][1]<y)) {
+        found.push(node.point)
+    }
+    range_query_rect(node.left, rect, found, depth+1)
+    range_query_rect(node.right, rect, found, depth+1)
 }
--- sketch.js
+++ sketch.js
@@ -1,37 +1,104 @@
-function setup () {
-var width = 250;
- var height = 200;
-createCanvas (width , height ) ;
- background (0) ;
-for (var x = 0; x < width; x += width / 10) {
-for (var y = 0; y < height; y += height / 5) {
-stroke (125 , 125 , 125) ;
-strokeWeight (1) ;
-line (x, 0, x, height );
-line (0 , y, width , y);
-}
-}
-var data = [];
-for ( let i = 0; i < 12; i ++) {
-var x = Math.floor ( Math.random () * height );
-var y = Math.floor ( Math.random () * height );
-data.push ([x, y]) ;
-fill (255 , 255 , 255) ;
-circle (x, height - y, 7) ; // 200 -y para q se dibuje apropiadamente
-textSize (8) ;
-text (x + ',' + y, x + 5, height - y);// 200 -y para q se dibuje apropiadamente
-}
-var root = build_kdtree ( data ) ;
-console.log ( root );
-}
-k = 2;
+function setup() {
+    var width = 250;
+    var height = 200;
+    let kdTreeCanvas = createCanvas(width, height);
+    kdTreeCanvas.parent("KdTreeCanvas");
+    background(0);
+    for (var x = 0; x < width; x += width / 10) {
+        for (var y = 0; y < height; y += height / 5) {
+            stroke(125, 125, 125);
+            strokeWeight(1);
+            line(x, 0, x, height);
+            line(0, y, width, y);
+        }
+    }
+    // var data = [];
+    // for (let i = 0; i < 12; i++) {
+    //     var x = Math.floor(Math.random() * height);
+    //     var y = Math.floor(Math.random() * height);
+    //     data.push([x, y]);
+    //     fill(255, 255, 255);
+    //     circle(x, height - y, 7); // 200 -y para q se dibuje apropiadamente
+    //     textSize(8);
+    //     text(x + ',' + y, x + 5, height - y);// 200 -y para q se dibuje apropiadamente
+    // }
+    var data = [
+        [40 ,70] ,
+        [70 ,130] ,
+        [90 ,40] ,
+        [110 , 100] ,
+        [140 ,110] ,
+        [160 , 100]
+    ];
 
-function distanceSquared ( point1 , point2 ){
-var distance = 0;
-for (var i = 0; i < k; i ++)
-distance += Math.pow (( point1 [i] - point2 [i]) , 2) ;
-return Math.sqrt ( distance );
- }
+    var data = [
+        [100 ,100] ,
+        [40 ,70] ,
+        [90 ,40] ,
+        [70 , 130] ,
+        [175 ,100] ,
+        [150 , 30],
+        [140, 110],
+    ];
+    for (let i = 0; i < data.length; i++) {
+        fill(255, 255, 255);
+        circle(data[i][0], height - data[i][1], 7); // 200 -y para q se dibuje apropiadamente
+        textSize(8);
+        text(data[i][0] + ',' + data[i][1], data[i][0] + 5, height - data[i][1]);// 200 -y para q se dibuje apropiadamente
+    }
+    
+    var point = [140 ,90]; // query
+    fill('red');
+    circle(point[0], height - point[1], 7); // 200 -y para q se dibuje apropiadamente
+    textSize(8);
+    text(point[0] + ',' + point[1], point[0] + 5, height - point[1]);
 
- function closest_point_brute_force ( points , point ) {}
- function naive_closest_point (node , point , depth = 0, best = null ) {}
+    var root = build_kdtree(data);
+    console.log(root);
+    console.log(generate_dot(root));
+
+    console.log(closest_point_brute_force( data , point ));
+    console.log(naive_closest_point ( root , point ));
+    console.log(closest_point ( root , point ).point);
+    KNN(data, point, 2);
+
+    // var found = [];
+	// var pon = [140, 100];
+	// var radio = 90;
+
+    // range_query_circle(root,pon,radio,found);
+    // console.log(found);
+	// fill(0, 0, 255, 40);
+	// circle(pon[0], height - pon[1], radio * 2);
+    // for ( let i = 0; i < found.length; i ++) {
+    //     var x = found[i][0];
+    //     var y = found[i][1];
+    //     fill (0 , 0 , 255);
+    //     circle (x, height - y, 7); 
+    // }
+	
+
+	stroke(0,0,255);
+    rectMode(CENTER);
+    fill(0, 0, 255, 40);
+    var rectWidth = 100
+    var rectHeight = 100
+
+    let center = [140,100];
+    rect(center[0], center[1], rectWidth, rectHeight);
+    var xMin = center[0] - rectWidth/2;
+    var yMin = height - (center[1] + rectHeight/2);
+    var xMax = center[0] + rectWidth/2;
+    var yMax = height - (center[1] - rectHeight/2);
+
+    let found = []
+    var rectangle = [[xMin, xMax], [yMin, yMax]]
+    range_query_rect(root, rectangle, found)
+    console.log(found)
+    for ( let i = 0; i < found.length; i ++) {
+        var x = found[i][0];
+        var y = found[i][1];
+        fill (0 , 0 , 255);
+        circle (x, height - y, 7); 
+    }
+}
